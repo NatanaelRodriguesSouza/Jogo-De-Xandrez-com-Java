234a6d902f6a6c1d4c97cc325075e8572ae2942f
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-07-17 19:46:44 -0300

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -193,6 +194,7 @@ public class ChessMatch {
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 8, new King(board, Color.BLACK));
+		placeNewPiece('a',2, new Pawn(board,Color.WHITE));
 
 	}
 }
