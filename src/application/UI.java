5eea6e5b72d72c4a935b5bdbc827d92ee0116a76
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-07-17 19:00:16 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -60,15 +60,22 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 	
-	public static void printMatch(ChessMatch chessMatch,List<ChessPiece> captured) {
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPiece());
 		System.out.println();
 		printCapturedPiece(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()){
-			System.out.println("CHECK");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
